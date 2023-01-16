5c0a88a6cf113277ea4063cb1fda1c3dd6bdd826
jardelrodriguespinto <ghmagno910@gmail.com>
2023-01-16 19:50:08 -0300

    Spring JPA project to pratice my knowlegde

---
+++ LearningSpringJPA-AM/src/main/java/com/springjpatutorial/LearningSpringJPA/LearningSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springjpatutorial.LearningSpringJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringJpaApplication.class, args);
+	}
+
+}
---
+++ LearningSpringJPA-AM/src/test/java/com/springjpatutorial/LearningSpringJPA/LearningSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjpatutorial.LearningSpringJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
